3095532938102afe60f37e43edaeee2165443710
Robson Correia <robson.correia.bd@gmail.com>
2022-09-21 15:33:06 -0300

    feat: ef in progress

---
+++ MAUI/Maui.Infrastructure/Configuration/EF/MauiContext.cs
@@ -0,0 +1,15 @@
+﻿using Maui.Entity.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.Infrastructure.Configuration.EF
+{
+    public class MauiContext : DbContext
+    { 
+        public DbSet<Project> Project { get; set; }
+    }
+}
