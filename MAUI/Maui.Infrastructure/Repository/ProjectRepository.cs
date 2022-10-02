b0b98f9c8fcd52d84a05166dcc48261f7e7e031e
Robson Correia <robson.correia.bd@gmail.com>
2022-10-02 02:00:07 -0300

    fix: project controller

--- MAUI/Maui.Infrastructure/Repository/ProjectRepository.cs
+++ MAUI/Maui.Infrastructure/Repository/ProjectRepository.cs
@@ -3,17 +3,60 @@ using Maui.Infrastructure.Configuration.EF;
 using Maui.Infrastructure.Repository.Generic;
 using Maui.Infrastructure.Repository.Interface;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using System.Linq.Expressions;
+using System.Text;
+using System.Text.Json;
 
 namespace Maui.Infrastructure.Repository
 {
     public class ProjectRepository : GenericRepository<ProjectModel>, IProjectRepository
     {
+        private readonly HttpClient _client;
+        private readonly JsonSerializerOptions _serializerOptions;
+
+        public async Task Add(ProjectModel project)
+        {
+            Uri uri = new Uri(string.Format("https://localhost:7058/api/Project", string.Empty));
+
+            try
+            {
+                string json = JsonSerializer.Serialize<ProjectModel>(project);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = null;
+
+                if (true/*isNewItem*/)
+                {
+                    response = await _client.PostAsync(uri, content);
+                }
+                else
+                {
+                    response = await _client.PutAsync(uri, content);
+                }
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine(@"\tTodoItem successfully saved.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+        }
+
         private readonly DbContextOptions<MauiContext> _dbContextOptions;
 
         public ProjectRepository()
         {
             _dbContextOptions = new DbContextOptions<MauiContext>();
+            _client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
         }
 
         public async Task<List<ProjectModel>> ListarUsuarios(Expression<Func<ProjectModel, bool>> expression)
---
+++ MAUI/Maui.WebApplication/Controllers/ProjectController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Maui.Entity.Entity;
+using Maui.Infrastructure.Configuration.EF;
+
+namespace Maui.WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly MauiContext _context;
+
+        public ProjectController(MauiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Project
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProjectModel>>> GetProject()
+        {
+            return await _context.Project.ToListAsync();
+        }
+
+        // GET: api/Project/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProjectModel>> GetProjectModel(int id)
+        {
+            var projectModel = await _context.Project.FindAsync(id);
+
+            if (projectModel == null)
+            {
+                return NotFound();
+            }
+
+            return projectModel;
+        }
+
+        // PUT: api/Project/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProjectModel(int id, ProjectModel projectModel)
+        {
+            if (id != projectModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(projectModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Project
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProjectModel>> PostProjectModel(ProjectModel projectModel)
+        {
+            _context.Project.Add(projectModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProjectModel", new { id = projectModel.Id }, projectModel);
+        }
+
+        // DELETE: api/Project/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProjectModel(int id)
+        {
+            var projectModel = await _context.Project.FindAsync(id);
+            if (projectModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Project.Remove(projectModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProjectModelExists(int id)
+        {
+            return _context.Project.Any(e => e.Id == id);
+        }
+    }
+}
